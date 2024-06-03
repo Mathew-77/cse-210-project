e94cf02c74b837c760a05eed1d857e539fd27fbd
mathew benard <mathewbenard707@gmail.com>
2024-06-03 13:02:44 -0700

    my final hw

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
+class video
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        private string _title;
+        private string _author;
+        private double _length;
+        private list <comment> _comment = new list<comment>();
+
+        public video (string _title,string _author, double _length)
+        {
+            _title = title;
+            _author = author;
+            _length = length;
+        }
+        public void list(comment, comment)
+        {
+            _comment . add(comment);
+        }
+        public int countcomment()
+        {
+            int count = 0;
+            _count = comment.count;
+            return count
+        }
+        public void displayinfo()
+        {
+            console.writeline($"title: {_title} Author: {_author} length {_length} seconds");
+            console.writeline();
+            int comment count =countcomments();
+            console.writeline($"number of comments: {commentcount}");
+            foreach (commentcount in _comment);
+            {
+                comment.displayinfo();
+            }
+            
+                
+            
+
+        }
+        
     }
 }
